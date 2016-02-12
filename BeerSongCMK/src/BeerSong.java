4efac9b12964fe62602da70b11de56e4248041af
chrisk <ckv9508@saintjoe.edu>
2016-02-12 13:54:35 -0500

    Finished Beer Song code -fixed bug

--- BeerSongCMK/src/BeerSong.java
+++ BeerSongCMK/src/BeerSong.java
@@ -1,10 +1,30 @@
 
+// This file is being edited by Christopher Kenessey
 public class BeerSong {
 
 	public static void main(String[] args) {
-		// This file is being edited by Christopher Kenessey
-		// TODO Auto-generated method stub
 
+		int beerNum = 99;
+		String word = "bottles";
+		while (beerNum > 0) {
+			if(beerNum == 1){
+				word = "bottle";
+			}
+		System.out.println(beerNum + " " + word + " of beer on the wall");
+		System.out.println(beerNum + " " + word + " of beer");
+		System.out.println("Take one down.");
+		System.out.println("Pass it around.");
+		beerNum = beerNum - 1;
+		}{
+			System.out.println("No more bottles of beer on the wall");
+		} // end else 
+		} // end while 
+	
+	{
+				
+			}
+	{
+		
 	}
 
 }
